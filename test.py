bfe3b358dde88ee6d660f283dcb48a1702d18d74
ShuiPP <shuipanpandl@126.com>
2017-09-28 16:02:41 +0800

    add one python file

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import keyword
+import sys
+print("Hello, World!")
+print(keyword.kwlist)  # 输出关键字
+paragraph = """这是一个段落，
+可以由多行组成"""
+x = 'runoob'
+sys.stdout.write(x + '\n')
+input("\n\n按下 enter 键后退出。")
+
+
